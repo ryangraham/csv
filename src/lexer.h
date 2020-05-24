91c277e1c40fd6fc2f63dcc6febdecb8a2e863f2
Ryan Graham <rgraham@blvd.co>
2020-05-23 19:21:33 -0700

    move token struct to token.h

--- src/lexer.h
+++ src/lexer.h
@@ -5,40 +5,11 @@
 #include <string>
 #include <vector>
 
-#define ENUM_LIST(X) \
-  X(COMMA)           \
-  X(IDENTIFIER)      \
-  X(END)
-
-#define AS_ENUM(e) e,
-
-#define AS_STRING(E) #E,
+#include "token.h"
 
 using namespace ranges;
 
 namespace csv {
-
-constexpr char const* token_type_names[] = {ENUM_LIST(AS_STRING)};
-enum struct token_type { ENUM_LIST(AS_ENUM) };
-char const* token_type_name(token_type const val) {
-  return token_type_names[static_cast<int>(val)];
-}
-
-struct token {
- public:
-  token(token_type type, const char* begin, const char* end)
-      : type{type}, value(begin, std::distance(begin, end)) {
-    type_str = token_type_name(type);
-  }
-  token(token_type type, const char* begin, int len)
-      : type{type}, value(begin, len) {
-    type_str = token_type_name(type);
-  }
-
-  token_type type{};
-  std::string_view type_str{};
-  std::string_view value{};
-};
 
 struct lexer {
  public:
